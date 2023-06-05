257a12d3f0b9ee919e16ec3687364faa6185ce1e
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-06-05 03:05:32 +0000

    Components & Databinding section

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from 'src/app/models/Recipe';
 
 @Component({
@@ -8,7 +8,12 @@ import { Recipe } from 'src/app/models/Recipe';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('Test recipe', 'This is a test', ''),
-    new Recipe('Test recipe', 'This is a test', '')
+    new Recipe('Test recipe', 'This is a test', 'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food.jpg'),
+    new Recipe('Another test recipe', 'This is a test', 'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food.jpg')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
